a5e5bf8d1a6249b5c9dd341d8f435d548deb6518
Adithyan <adithyanp@crayondata.com>
2025-09-29 12:35:44 +0100

    adding first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+![example workflow](https://github.com/gclynch/helloworldjava/actions/workflows/gradle.yml/badge.svg)
---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class HelloWorldTest 
+{
+    @Test
+    public void testAdd() throws Exception 
+	{
+        assertEquals(3, 2 + 1);
+    }
+}
